d8a1c6af3b945848991337a9407eda6e342afb7b
jerkjoe <jinyuhui61@hotmail.com>
2020-03-08 06:29:26 +0000

    Deploying to gh-pages from  - 374ed3c9c365d3833cf16cb1f3494beb37b005e8 🚀

---
+++ precache-manifest.61b7e77f15b129f27f1f5f5802550d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cde1aed82537955dfd261b2c6bfd431",
+    "url": "/BLUE/index.html"
+  },
+  {
+    "revision": "43f13bd00ae7fd9d6977",
+    "url": "/BLUE/static/css/main.486a5b50.chunk.css"
+  },
+  {
+    "revision": "9db3f3761c350ed9aa4d",
+    "url": "/BLUE/static/js/2.95989cdb.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/BLUE/static/js/2.95989cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f13bd00ae7fd9d6977",
+    "url": "/BLUE/static/js/main.7eedaf0c.chunk.js"
+  },
+  {
+    "revision": "c2c16678b4594781ebf5",
+    "url": "/BLUE/static/js/runtime-main.17cfcb4b.js"
+  }
+]);
